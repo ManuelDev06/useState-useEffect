a5d7e73e6222c97c9f6dd7b164bf860c7d05b3eb
ManuelDev06 <manuel.barba@improving.com>
2022-11-02 12:01:28 -0600

    first commit

---
+++ src/components/UseState.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const UseState = () => {
+
+  const [state, setstate] = useState(0);
+
+  return (
+    <div>UseState</div>
+  )
+}
+
+export default UseState
